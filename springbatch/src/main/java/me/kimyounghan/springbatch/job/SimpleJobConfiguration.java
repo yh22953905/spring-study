ed89d4d89946477b7f2b6d4786a09e4cf3b99635
yh22953905 <yh22953905@gmail.com>
2022-08-28 19:58:20 +0900

    feat: Spring Batch 학습 - Spring Batch 가이드 - Batch Job 실행해보기 : 프로젝트 생성 및 Job, Step, Tasklet 생성 - 출처: https://jojoldu.tistory.com/325?category=902551

---
+++ springbatch/src/main/java/me/kimyounghan/springbatch/job/SimpleJobConfiguration.java
@@ -0,0 +1,38 @@
+package me.kimyounghan.springbatch.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration // Spring Batch의 모든 Job은 @Configuration으로 등록해서 사용
+public class SimpleJobConfiguration {
+    private static final String SIMPLE_JOB = "simpleJob";
+    private static final String SIMPLE_STEP_1 = "simpleStep1";
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job simpleJob() {
+        return jobBuilderFactory.get(SIMPLE_JOB) // simpleJob이란 이름의 Batch Job을 생성, job의 이름은 별도로 지정하지 않고, 이렇게 Builder를 통해 지정
+            .start(simpleStep1())
+            .build();
+    }
+
+    @Bean
+    public Step simpleStep1() {
+        return stepBuilderFactory.get("simpleStep1") // simpleStep 이란 이름의 Batch Step 을 생성, step의 이름은 별도로 지정하지 않고, 이렇게 Builder를 통해 지정
+            .tasklet(((contribution, chunkContext) -> { // Step 안에서 수행될 기능들 명시, asklet은 Step안에서 단일로 수행될 커스텀한 기능들을 선언
+                log.info(">>>>> This is Step1");
+                return RepeatStatus.FINISHED;
+            })).build();
+    }
+}
